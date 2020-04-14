5347e3e3b15a4a0f9fcdc09400ed8aa7ee03c8f0
michalmw <michal@surprise.design>
2020-04-14 15:19:20 +0200

    Private component

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { PrivateComponent } from './private/private.component';
 
 const routes: Routes = [
   {
-    path: 'users',
-    loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
-  },
-  {
-    path: 'clients',
-    loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule)
+    path: '',
+    component: PrivateComponent,
+    children: [
+      {
+        path: 'users',
+        loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
+      },
+      {
+        path: 'clients',
+        loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule)
+      }
+    ]
   }
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [PrivateComponent],
   imports: [
     RouterModule.forChild(routes),
     CommonModule
