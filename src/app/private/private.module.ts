2143bd7fdd69481e6ba86b0182bc52c37d910ca2
Natalia <ntlbrwck@gmail.com>
2020-04-15 12:47:59 +0200

    menu sidenav

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -1,9 +1,12 @@
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PrivateComponent } from './private/private.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [
   {
@@ -29,7 +32,10 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes),
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class PrivateModule { }
