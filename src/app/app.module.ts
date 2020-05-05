eb02fe7e544fa38f5e3a3d8940fb021c1e41f16d
michalmw <michal@surprise.design>
2020-05-05 16:24:27 +0200

    Init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 @NgModule({
@@ -21,7 +22,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/private/tickets/edit/edit.component.ts
+++ src/app/private/tickets/edit/edit.component.ts
@@ -21,11 +21,12 @@ export class EditComponent implements OnInit {
   form = new FormGroup({
     id: new FormControl(null),
     user: new FormControl(null, [Validators.required]),
+    description: new FormControl(null),
     client: new FormControl(null, [Validators.required]),
     title: new FormControl(null, [Validators.required]),
     status: new FormControl(),
   });
-  commentform = new FormGroup({
+  commentForm = new FormGroup({
     date: new FormControl(),
     commentuser: new FormControl(),
     description: new FormControl(),
@@ -35,7 +36,7 @@ export class EditComponent implements OnInit {
   users = [];
   clients = [];
   comments = [];
-  addcomment: boolean = false;
+  addcomment = false;
 
   date = new FormControl(new Date());
   serializedDate = new FormControl(new Date().toISOString());
@@ -56,36 +57,64 @@ export class EditComponent implements OnInit {
     this.getUsers();
     this.getClients();
   }
+
+  addComments() {
+    this.addcomment = true;
+    const loggedUserId = +localStorage.getItem('userId');
+    this.commentForm.patchValue({commentuser: this.users.find(user => user.id === loggedUserId)});
+    ;
+  }
+
   getTicket() {
     this.ticketsService.getTicket(this.id).subscribe((ticket: Ticket) => {
       this.form.patchValue(ticket);
       this.ticket = ticket;
-      this.comments = ticket.comments;
+      if (ticket.comments) {
+        this.comments = ticket.comments;
+      }
     });
   }
+
   getUsers() {
     this.usersService
       .getUsers()
       .subscribe((listOfUsers: any) => (this.users = listOfUsers));
   }
+
   getClients() {
     this.clientsService
       .getClients()
       .subscribe((listOfClients: any) => (this.clients = listOfClients));
   }
-  save() {
+
+  save(goToList = true) {
     const dataToSave = {
       ...this.form.value,
+      comments: this.comments
     };
     this.ticketsService.edit(dataToSave).subscribe(() => {
       this.snackBar.open('Zapisano zgłoszenie', '', {
         duration: 2000,
       });
-      this.router.navigate(['app/tickets']);
+      if (goToList) {
+        this.router.navigate(['app/tickets']);
+      }
     });
   }
-  savecomment() {
-    this.comments.push(this.commentform.value);
-    
+
+  saveComment() {
+    this.comments.push(this.commentForm.value);
+    this.save(false);
   }
+  // Problem 1: => Data dziwnie sie pokazuje
+  // Pipe | data => w odpowiedniej formie np DD-MM-YYYY || dd/mm/yyyy
+
+  // Problem 2 => schowac formularz po dodaniu
+  // Problem 3 => aby sie czyscil
+
+  // Problem 4 => Edycja
+  // Problem 5 => Usuwanie
+
+  // Problem 6 => data moge wybrac pozniejsza (material-angular dokumentacja)
+  // Problem 7 => uzytkownika
 }
