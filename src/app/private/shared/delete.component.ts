424cef8bbc57f7296e1d2e44f84192ec7fc03821
Natalia <ntlbrwck@gmail.com>
2020-05-11 16:10:26 +0200

    comments

--- src/app/private/shared/delete.component.ts
+++ src/app/private/shared/delete.component.ts
@@ -3,7 +3,13 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-delete',
-  template: './delete.component.html'
+  template: `Czy napewno chcesz usunac? <br />
+    <button mat-button (click)="onNoClick()">
+      Nie
+    </button>
+    <button mat-button[mat-dialog-close]="data.isConfirm" cdkFocusInitial>
+      Ok
+    </button> `,
 })
 export class DeleteComponent implements OnInit {
   constructor(
